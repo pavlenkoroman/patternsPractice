9196f24d8e4f9daf666a55e9b56f24a03e224287
Roman <romapavlenkod@gmail.com>
2021-10-06 19:58:38 +0300

    Singleton

---
+++ Singleton/Singleton/ChocolateBoiler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class ChocolateBoiler
+    {
+        private static ChocolateBoiler boiler;
+
+        private static object syncFlag;
+
+        public bool Empty { get; private set; }
+        public bool Boiled { get; private set; }
+
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public ChocolateBoiler GetBoiler()
+        {
+            if (boiler == null)
+            {
+                lock (syncFlag)
+                {
+                    boiler = new ChocolateBoiler();
+                }
+            }
+            return boiler;
+        }
+
+        public void Fill()
+        {
+            if (Empty)
+            {
+                Empty = false;
+                Boiled = false;
+            }
+        }
+        public void Boil()
+        {
+            if (!Empty && !Boiled)
+            {
+                Boiled = true;
+            }
+        }
+        public void Drain()
+        {
+            if (!Empty && Boiled)
+            {
+                Empty = true;
+                Boiled = false;
+            }
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
