99991346b554cb0a9248bab00e0de493cb822500
Roman <romapavlenkod@gmail.com>
2021-11-20 10:47:00 +0200

    Mediator

---
+++ Mediator/Mediator/Mediator/ProjectManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using Mediator.MediatorUsers;
+
+namespace Mediator.Mediator
+{
+    public class ProjectManager : Manager
+    {
+        public Designer Designer { get; set; }
+        public Programmer Programmer { get; set; }
+        public override void Send(string message, OfficeWorker officeWorker)
+        {
+            if (Designer == officeWorker)
+            {
+                Programmer.Notify(message);
+            }
+            else
+            {
+                Designer.Notify(message);
+            }
+        }
+    }
+}
---
+++ Mediator/Mediator/Program.cs
@@ -0,0 +1,21 @@
+﻿using Mediator.Mediator;
+using Mediator.MediatorUsers;
+
+namespace Mediator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProjectManager pm = new ProjectManager();
+            Programmer programmer = new Programmer(pm);
+            Designer designer = new Designer(pm);
+
+            pm.Programmer = programmer;
+            pm.Designer = designer;
+            
+            designer.Send("Дизайн готов, иди работай");
+            programmer.Send("Моя часть работы окончена, жду следующий макет");
+        }
+    }
+}
